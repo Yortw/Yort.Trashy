ee360467d66327623ce229409c1531568e044299
Troy Willmot <yort@yortondotnet.com>
2018-01-24 21:57:33 +1300

    Add NeutralResourcesLanguage attribute to make UWP packages happy. Fix default namespace for net40 project. Update copyright. Add editorconfig file. Add contributing.md Change netstanadard project to use assemblyinfocommon.cs for consistency. Version bump. Add .nuget to .gitignore.

--- Src/Yort.Trashy.Shared/AssemblyInfoCommon.cs
+++ Src/Yort.Trashy.Shared/AssemblyInfoCommon.cs
@@ -1,17 +1,16 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
-#if !NETSTANDARD1_0
 [assembly: AssemblyProduct("Yort.Trashy")]
 [assembly: AssemblyDescription("Types, interfaces & extension methods for creating or dealing with disposable resources in .Net.")]
 
 [assembly: ComVisible(false)]
 
 //Do not forget to update version # in netstandard project properties too
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-#endif
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -21,5 +20,6 @@ using System.Runtime.InteropServices;
 #else
 [assembly: AssemblyConfiguration("RELEASE")]
 #endif
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: System.CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en")]
